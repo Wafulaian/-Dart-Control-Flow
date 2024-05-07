cbcfd50fb29a425fd9048af8a5867fc486ff5b66
Wafulaian <137065400+Wafulaian@users.noreply.github.com>
2024-05-07 10:43:16 +0300

    Add files via upload

---
+++ dart_application_3.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Replace this with a mechanism to get user input (e.g., function)
+  int number = 15; // Replace with user input
+
+  // Check the number and print the message
+  if (number > 10) {
+    print("Your number is greater than 10");
+  } else if (number < 10) {
+    print("Your number is less than 10");
+  } else {
+    print("Your number is equal to 10");
+  }
+}
